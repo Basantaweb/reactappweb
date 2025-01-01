1f18d0f74aae5115af317853da7d9db980456490
Basanta <basantamannanew@gmail.com>
2025-01-01 17:53:35 +0530

    add new

--- src/--index.jsx
+++ src/--index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Apps() {
   return (
     <div>
       <h1>Hello, React!</h1>
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default Apps;
